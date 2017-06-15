468f59be152a127e9252e1cb0252d0b82c79c16c
jun.chu <1571970439@qq.com>
2017-06-15 20:51:18 +0800

    add kibana,redis sh

---
+++ mysh/kibana-5.4.0.sh
@@ -0,0 +1,3 @@
+cd /Users/chujun/Documents/mycmd/kibana-5.4.0-darwin-x86_64/bin
+echo 'mac kibana-5.4.0 start ,default Server running at http://localhost:5601'
+./kibana
---
+++ mysh/redis-server.sh
@@ -0,0 +1,3 @@
+cd /usr/local/Cellar/redis/3.2.9
+echo 'redis-server start ,port 6379'
+redis-server
