f4c5dcf9496c0e91c40301fb52e44cb66b4e98ea
jun.chu <1571970439@qq.com>
2020-02-28 09:49:49 +0800

    add git/gitGlobalConfig.sh

---
+++ git/gitGlobalConfig.sh
@@ -0,0 +1,11 @@
+echo '--author name is "jun.ch"'
+git config --global alias.lm  "log --color --date=format:'%Y-%m-%d %H:%M:%S' --author='jun.chu' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+git config --global alias.lms  "log --color --stat --date=format:'%Y-%m-%d %H:%M:%S' --author='jun.chu' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+git config --global alias.ls "log --color --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+
+git config --global alias.lss "log --color --stat --graph --date=format:'%Y-%m-%d %H:%M:%S' --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Cblue %s %Cgreen(%cd) %C(bold blue)<%an>%Creset' --abbrev-commit"
